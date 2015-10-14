4c2d3a48cecb72513c6100bcbe3af29c1f2d4045
aleksandr_simkin <aleksandr.simkin@db.com>
2015-10-14 15:03:21 +0300

    unit tests

---
+++ src/test/java/github/templates/poc/controllers/FreemarkerControllerTest.java
@@ -0,0 +1,59 @@
+package github.templates.poc.controllers;
+
+import github.templates.poc.TemplatesPocApplication;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TemplatesPocApplication.class)
+@WebAppConfiguration
+public class FreemarkerControllerTest {
+
+    @Autowired private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void testGetTemplateNoTemplate() throws Exception {
+        mockMvc.perform(get("/freemarker"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    public void testUpdateTemplate() throws Exception {
+        mockMvc.perform(post("/freemarker")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"__template__\":\"template\"}"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    public void testUpdateTemplateBadTemplate() throws Exception {
+        mockMvc.perform(post("/freemarker")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"__template__\":\"template ${olo\"}"))
+                .andExpect(status().isBadRequest());
+    }
+
+}

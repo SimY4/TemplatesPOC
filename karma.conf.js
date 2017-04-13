25891a0ea0319d604e125dbc4d5282afbec30d33
Alex Simkin <alex.simkin@outlook.com>
2017-04-13 23:44:13 +1000

    controller test added

--- karma.conf.js
+++ karma.conf.js
@@ -23,7 +23,7 @@ module.exports = function(config) {
 
         plugins: [
             'karma-chrome-launcher',
-            // 'karma-firefox-launcher',
+            'karma-firefox-launcher',
             'karma-jasmine'
         ]
     });

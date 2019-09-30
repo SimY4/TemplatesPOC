1b7717af989b01afd504752be6888a9c17a4b906
SimY4 <alex.simkin@outlook.com>
2019-09-30 21:20:08 +1000

    Minor fixes.

--- src/webapp/js/common/codemirror.service.js
+++ src/webapp/js/common/codemirror.service.js
@@ -1,8 +1,7 @@
 'use strict';
 
 angular.module('templates.common').factory('CodeMirrorNg', [function () {
-    var CodeMirrorNg = function (element, options) {
+    return function (element, options) {
         return CodeMirror.fromTextArea(document.getElementById(element), options);
     };
-    return CodeMirrorNg;
 }]);

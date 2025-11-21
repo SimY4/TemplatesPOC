061bdb9cb36aff903c92f04bea5f470506331a9a
SimY4 <alex.simkin@outlook.com>
2025-11-21 15:38:11 +1100

    Testing it with new modules.

--- src/test/kotlin/com/github/simy4/poc/IntegrationTest.kt
+++ src/test/kotlin/com/github/simy4/poc/IntegrationTest.kt
@@ -3,10 +3,12 @@ package com.github.simy4.poc
 import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
 
+@AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 abstract class IntegrationTest {
   protected lateinit var mockMvc: MockMvc
